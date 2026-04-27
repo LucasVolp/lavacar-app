fc4be657303a9ef94a0adb2876491294c9bfd604
LucasVolp <lucasgabrielvmb@gmail.com>
2026-04-27 16:19:19 -0400

    fix: update logo URLs in BRAND_CONFIG for consistency

--- config/constants.ts
+++ config/constants.ts
@@ -1,8 +1,8 @@
 export const BRAND_CONFIG = {
   CDN_URL: "https://cdn.nexocar.com.br",
   LOGO_SMALL_URL: "https://cdn.nexocar.com.br/system/assets/logo2-preview.png",
-  LOGO_LARGE_URL: "https://cdn.nexocar.com.br/system/assets/logo.png",
-  FAVICON_URL: "https://cdn.nexocar.com.br/system/assets/logo.png",
+  LOGO_LARGE_URL: "https://cdn.nexocar.com.br/system/assets/logo-favicon2.png",
+  FAVICON_URL: "https://cdn.nexocar.com.br/system/assets/logo-favicon.png",
   COMPANY_NAME: "NexoCar",
   COMPANY_EMAIL: "contato@nexocar.com",
 };
